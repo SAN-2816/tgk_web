01012a1c04fdf446a633253de89038f18f0d29af
SAN-2816 <jinsan2816@gmail.com>
2020-02-07 14:47:51 +0900

    Updates

---
+++ precache-manifest.b4f3600270aa9e6f1d578dafdcfe1f0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ba0d4b7c6071669f30847f6b768547",
+    "url": "/tgk_web/index.html"
+  },
+  {
+    "revision": "865987ed8879c955d039",
+    "url": "/tgk_web/static/css/main.b413df2b.chunk.css"
+  },
+  {
+    "revision": "2165a28cee2ad9158a37",
+    "url": "/tgk_web/static/js/2.19352a14.chunk.js"
+  },
+  {
+    "revision": "5455a644ad04f3fe306d0df919e9daa8",
+    "url": "/tgk_web/static/js/2.19352a14.chunk.js.LICENSE"
+  },
+  {
+    "revision": "865987ed8879c955d039",
+    "url": "/tgk_web/static/js/main.e59cd4c6.chunk.js"
+  },
+  {
+    "revision": "c448df478f3d23f69096",
+    "url": "/tgk_web/static/js/runtime-main.0689825e.js"
+  },
+  {
+    "revision": "3e39a2ac404b77db2c16afdfc11ff5a8",
+    "url": "/tgk_web/static/media/logo512.3e39a2ac.png"
+  }
+]);
